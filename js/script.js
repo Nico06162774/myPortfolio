3464fb9f4491680db7f93a31f23810fced2b328c
Nico06162774 <130480469+Nico06162774@users.noreply.github.com>
2023-05-14 15:14:34 +0800

    14/052023

--- js/script.js
+++ js/script.js
@@ -8,5 +8,26 @@ $(document).ready(function(){
 	$(window).on('scroll load', function(){
 		$('#menu').removeClass('fa-times');
 		$('header').removeClass('toggle');
-	})
+	});
+
+	$('a[href*="#"]').on('click', function(e){
+		e.preventDefault()
+
+		$('html,body').animate({
+			scrollTop : $($(this).attr('href')).offset().top,
+		}, 500, 'linear'
+		);
+	});
+
+
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx5Ev4L20L6icGNQB4MzI3DTNK6HcdCNpnMqWeWWX3QwU9XUI3HOs1S-vTD-ri8bKwq/exec'
+	const form = document.forms['submit-to-google-sheet']
+  
+	form.addEventListener('submit', e => {
+	  e.preventDefault()
+	  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+		.then(response => console.log('Success!', response),form.reset())
+		.catch(error => console.error('Error!', error.message))
+	});
